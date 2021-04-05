a032283b234016d0428b6e9f90b36356efe63ffe
DinhSonHai <sdinhhai@gmail.com>
2021-04-05 16:24:27 +0700

    Connect project to DB

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
 app.use(express.json());
 
+// Connect to DB
+connectDB();
+
 const PORT = process.env.PORT || 5000;
 
+// Root API
 app.get('/', (req, res) => {
   res.json({ message: 'Server is running'});
 });
